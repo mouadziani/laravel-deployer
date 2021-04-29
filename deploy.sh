1e3f5a656cc78ea84e455b4267c8f13a37f4beee
MouadZIANI <mouad.ziani1997@gmail.com>
2021-04-29 16:19:21 +0000

    feat: added required commands

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+# Turn on maintenance mode
+php artisan down || true
+
+# Pull the latest changes from the git repository
+# git reset --hard
+# git clean -df
+git pull origin master
+
+# Install/update composer dependecies
+php composer.phar install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+# Restart FPM
+( flock -w 10 9 || exit 1
+    echo 'Restarting FPM...'; sudo -S service $FORGE_PHP_FPM reload ) 9>/tmp/fpmlock
+
+# Run database migrations
+php artisan migrate --force
+
+# Clear caches
+php artisan cache:clear
+
+# Clear expired password reset tokens
+php artisan auth:clear-resets
+
+# Clear and cache routes
+php artisan route:cache
+
+# Clear and cache config
+php artisan config:cache
+
+# Clear and cache views
+php artisan view:cache
+
+# Install node modules
+npm i
+
+# Build assets using Laravel Mix
+npm run production
+
+# Turn off maintenance mode
+php artisan up
